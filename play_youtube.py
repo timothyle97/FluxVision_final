4fdd20797428c760ea75e1f500a81432db22faf7
Timothy Le <timothyle97@gmail.com>
2022-08-03 08:43:09 -0700

    Update play_youtube.py
    Update indentation for consistency across all files (tab for each indentation). Modified omxplayer command to output audio to alsa.

--- play_youtube.py
+++ play_youtube.py
@@ -31,71 +31,71 @@ class videoinfo(object):
 
 #open playlist file
 try:
-    with open(abs_path + '.playlist_dat') as f:
-        playlist = pickle.load(f)
+	with open(abs_path + '.playlist_dat') as f:
+		playlist = pickle.load(f)
 except Exception, e:
-    playlist = []
+	playlist = []
 
 #open blacklist.txt file
 try:
-    with open(abs_path + 'blacklist.txt') as f:
-        blacklist = f.readlines()
+	with open(abs_path + 'blacklist.txt') as f:
+		blacklist = f.readlines()
 except Exception, e:
 	blacklist = []
 
 for video in playlist:
-    #skip video if the title contains a line in blacklist.txt
-    skipVideo = False
-    for line in blacklist:
-        if line[:-1] in video.title:
-            skipVideo = True
-            break
-    if skipVideo:
-        continue
-    
-    #adjust volume based on times in config.txt
-    if (int(mute_time) > int(unmute_time)): # if mute happens before midnight
-        if int(strftime("%-H%M", localtime())) > int(mute_time) or int(strftime("%-H%M", localtime())) < int(unmute_time):
-            #mute
-            vol = -6000
-        else:
-            vol = default_volume
-    else: # if mute happens after midnight
-        if int(strftime("%-H%M", localtime())) > int(mute_time) and int(strftime("%-H%M", localtime())) < int(unmute_time):
-            vol = -6000
-        else:
-            vol = default_volume
-
-    #if video file exists
-    if os.path.isfile(abs_path + video.filename):
-        #update .count_video
-        with open(abs_path + '.count_video') as count_video:
-            value = int(count_video.read())
-        with open(abs_path + '.count_video', 'w') as count_video:
-            count_video.write(str(value + 1))
-        #update .title_txt
-        with open(abs_path + '.title_txt', 'w') as title:
-            title.truncate()
-            title.write(video.title)
-        #update /etd/motd (displays current video on login)
-        if dashboard == 'y':
-            update(video.title)
-            #update ticker
-        if use_ticker == 'y':
-            #kill ticker.py (multiple instances if necessary)
-            pids = check_output(["pgrep","-f","ticker.py"])
-            for pid in pids.splitlines():
-                try:
-                    os.system("sudo kill %d"%(int(pid)))
-                except Exception,exc:
-                    print str(exc)
-                    #relaunch ticker.py
-                    try:
-                        subprocess.Popen("sudo python /home/pi/ticker.py &", shell=True)
-                    except Exception,exc:
-                        print str(exc)
-        #play video
-        process = subprocess.call(['omxplayer', '-b', '-o', 'local', abs_path + video.filename, '--vol', str(vol), '--no-osd'], stdout=open(os.devnull, 'wb'))
+	#skip video if the title contains a line in blacklist.txt
+	skipVideo = False
+	for line in blacklist:
+		if line[:-1] in video.title:
+			skipVideo = True
+			break
+	if skipVideo:
+		continue
+		
+	#adjust volume based on times in config.txt
+	if (int(mute_time) > int(unmute_time)): # if mute happens before midnight
+		if int(strftime("%-H%M", localtime())) > int(mute_time) or int(strftime("%-H%M", localtime())) < int(unmute_time):
+			#mute
+			vol = -6000
+		else:
+			vol = default_volume
+	else: # if mute happens after midnight
+		if int(strftime("%-H%M", localtime())) > int(mute_time) and int(strftime("%-H%M", localtime())) < int(unmute_time):
+			vol = -6000
+		else:
+			vol = default_volume
+			
+	#if video file exists
+	if os.path.isfile(abs_path + video.filename):
+		#update .count_video
+		with open(abs_path + '.count_video') as count_video:
+			value = int(count_video.read())
+		with open(abs_path + '.count_video', 'w') as count_video:
+			count_video.write(str(value + 1))
+		#update .title_txt
+		with open(abs_path + '.title_txt', 'w') as title:
+			title.truncate()
+			title.write(video.title)
+		#update /etd/motd (displays current video on login)
+		if dashboard == 'y':
+			update(video.title)
+		#update ticker
+		if use_ticker == 'y':
+			#kill ticker.py (multiple instances if necessary)
+			pids = check_output(["pgrep","-f","ticker.py"])
+			for pid in pids.splitlines():
+				try:
+					os.system("sudo kill %d"%(int(pid)))
+				except Exception,exc:
+					print str(exc)
+					#relaunch ticker.py
+					try:
+						subprocess.Popen("sudo python /home/pi/ticker.py &", shell=True)
+					except Exception,exc:
+						print str(exc)
+		#play video
+		process = subprocess.call(['omxplayer', '-b', '-o', 'alsa', abs_path + video.filename, '--vol', str(vol), '--no-osd'], stdout=open(os.devnull, 'wb'))
 
 #increment .count_playlist
 with open(abs_path + '.count_playlist') as count_playlist:
